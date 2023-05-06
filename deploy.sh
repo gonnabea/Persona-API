fa6dc14e8af3f2957a00c07294e87c9dbd5d246f
gonnabea <winning1993@naver.com>
2023-05-06 23:42:40 +0900

    [feat] github actions + shellScript를 활용한 배포 자동화 시도 (31)

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 
 echo "Automate Deploy Persona API..."
 # cd ~/desktop/projects/oracle-cloud
-SSH_KEY_2022_08_17
 # ssh opc@146.56.145.39 -i ssh-key-2022-08-17.key << EOF
 ssh opc@146.56.145.39 -i $SSH_KEY_2022_08_17 << EOF
 if [ -d "persona" ]; then
